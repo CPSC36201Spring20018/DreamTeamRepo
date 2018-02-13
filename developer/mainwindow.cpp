8d3b1f3aa68e4c7b6ab85d521f05d442a7b54cb4
Adam Weesner <klaviam@gmail.com>
2018-02-13 14:51:27 -0800

    Add files via upload

--- developer/mainwindow.cpp
+++ developer/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QDebug"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,50 +10,99 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //Database
     db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("../Database/database.sqlite");
+    db.setDatabaseName("../database/database.sqlite");
 
     if(db.open())
         qDebug() << "Database connected\n";
     else
         qDebug() << "Database NOT connected\n";
 
-    InitializeCalendar();
+    InitializeDb();
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::InitializeCalendar() {
-    QSqlQuery query(db);
-    query.prepare("SELECT * FROM database ORDER BY HOUR");
 
-    if(query.exec()) {
-        while(query.next()) {
-            qDebug() << query.value(0).toInt();
-            qDebug() << query.value(1).toInt();
-            qDebug() << query.value(2).toInt();
-            qDebug() << query.value(3).toInt();
-            qDebug() << query.value(4).toInt();
-            qDebug() << query.value(5).toInt();
-            qDebug() << query.value(6).toInt();
-            qDebug() << query.value(7).toInt();
-            qDebug() << query.value(8).toInt();
-            qDebug() << query.value(9).toInt();
-            qDebug() << query.value(10).toInt();
-            qDebug() << query.value(11).toInt();
-            qDebug() << query.value(12).toInt();
-            qDebug() << query.value(13).toInt();
-            qDebug() << query.value(14).toInt();
-            qDebug() << query.value(15).toInt();
-            qDebug() << query.value(16).toInt();
-            qDebug() << query.value(17).toInt();
-            qDebug() << query.value(18).toInt();
-            qDebug() << query.value(19).toInt();
-            qDebug() << query.value(20).toInt();
-            qDebug() << query.value(21).toInt();
-            qDebug() << query.value(22).toInt();
-            qDebug() << query.value(23).toInt();
+/*****************************************************************************
+ * InitializeDb
+ *____________________________________________________________________________
+ * This method will initialize the database. It will find a "1" in the
+ * database, then create another "1" for the current hour. It will then count
+ * how many hours passed between the first 1 and the current 1. (countHrs)
+ *****************************************************************************/
+void MainWindow::InitializeDb() {
+    QSqlQuery query(db);                                        //initial query
+    QString curDay = QDate::currentDate().toString().mid(0,3);  //current day name (Mon, Tue...Sun)
+    QTime curHour = QTime::currentTime();                       //current hour (12,13...23)
+    hour = 0;
+    countHrs = -1;
+
+    //Searches for the ealier "1", then updates the hour to how many hours passed, the dayStr to ealier day,
+    SearchDb();
+
+    qDebug() << day << " - " << hour << " - " << dayStr << " - " << countHrs << " - " << (hour-countHrs);
+
+    //Removes 1 from ealier hour
+    query.exec("UPDATE database SET '" + dayStr + "'=0 WHERE hour=" + QString::number(hour-countHrs));
+
+    //Finds amount of hours passed between ealier hour and current hour (countHrs)
+    if (countHrs != -1)
+        countHrs += curHour.hour() - hour;
+    //If countHrs is -1, then that means program is running for first time.
+    else
+        countHrs = 0;
+
+    qDebug() << "Hours Passed Since Last Update: " << countHrs;
+
+    //Updates a cell from the database. Selects the current day, then to current hour, then sets cell to 1
+    query.exec("UPDATE database SET '" + curDay + "'=1 WHERE hour=" + QString::number(curHour.hour()));
+}
+
+
+/*****************************************************************************
+ * SearchDb
+ *____________________________________________________________________________
+ * This method will search the database for a "1" integer.
+ *      hour = how many hours passed
+ *      dayStr = ealier day
+ *****************************************************************************/
+void MainWindow::SearchDb() {
+    QSqlQuery query(db);                                        //initial query
+
+    //Checks for where "1" is in the database.
+    query.prepare("SELECT * FROM database ORDER BY hour");
+
+    //First checks for previous 1 (from ealier update). Once it has place of 1, then counts each hour until current hour.
+    for (day = 1; day <= 7; day++) {
+        qDebug() << "\nNEXT DAY\n";
+        if(query.exec()) {
+            while(query.next()) {
+                qDebug() << query.value(day).toInt();
+                if (query.value(day).toInt() == 1) {
+                    qDebug() << "Found";
+
+                    switch(day) {
+                        case 1 : dayStr = "Mon"; countHrs = 0;
+                            break;
+                        case 2 : dayStr = "Tue"; countHrs = 24;
+                            break;
+                        case 3 : dayStr = "Wed"; countHrs = 48;
+                            break;
+                        case 4 : dayStr = "Thu"; countHrs = 72;
+                            break;
+                        case 5 : dayStr = "Fri"; countHrs = 96;
+                            break;
+                        case 6 : dayStr = "Sat"; countHrs = 120;
+                            break;
+                        default: dayStr = "Sun"; countHrs = 144;
+                            break;
+                    }
+                    return;
+                }
+                hour++;
+            }
         }
     }
 }
--- developer/mainwindow.h
+++ developer/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QtSql>
+#include <QTime>
 
 namespace Ui {
 class MainWindow;
@@ -17,7 +18,8 @@ public:
     ~MainWindow();
 
 private slots:
-    void InitializeCalendar();
+    void InitializeDb();
+    void SearchDb();
 
     //HOME
     void on_hb_view_clicked();
@@ -32,6 +34,10 @@ private slots:
 private:
     Ui::MainWindow *ui;
     QSqlDatabase db;
+    QString dayStr;
+    int day;
+    int hour;
+    int countHrs;
 };
 
 #endif // MAINWINDOW_H
